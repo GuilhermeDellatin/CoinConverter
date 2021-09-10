dbfdd676aac057a3dacff147c84f39cfe73e9842
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-09 22:01:49 -0300

    Prepare and Create UseCase and Extensions

---
+++ app/src/main/java/com/gfdellatin/coinconverter/core/extensions/Dialog.kt
@@ -0,0 +1,26 @@
+package com.gfdellatin.coinconverter.core.extensions
+
+import androidx.appcompat.app.AlertDialog
+import android.content.Context
+import android.widget.ProgressBar
+import com.gfdellatin.coinconverter.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun Context.createDialog(block: MaterialAlertDialogBuilder.() -> Unit = {}): AlertDialog {
+    val builder = MaterialAlertDialogBuilder(this)
+    builder.setPositiveButton(android.R.string.ok, null)
+    block(builder)
+    return builder.create()
+}
+
+fun Context.createProgressDialog(): AlertDialog {
+    return createDialog {
+        val padding = this@createProgressDialog.resources.getDimensionPixelOffset(R.dimen.layout_padding)
+        val progressBar = ProgressBar(this@createProgressDialog)
+        progressBar.setPadding(padding, padding, padding, padding)
+        setView(progressBar)
+
+        setPositiveButton(null, null)
+        setCancelable(false)
+    }
+}
