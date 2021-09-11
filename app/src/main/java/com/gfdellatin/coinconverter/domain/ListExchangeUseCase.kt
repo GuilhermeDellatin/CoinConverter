c9de980e8fb7625e5f81b4787e220dd15882f022
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-11 18:45:59 -0300

    Create UseCases for Database

---
+++ app/src/main/java/com/gfdellatin/coinconverter/domain/ListExchangeUseCase.kt
@@ -0,0 +1,19 @@
+package com.gfdellatin.coinconverter.domain
+
+import com.gfdellatin.coinconverter.core.UseCase
+import com.gfdellatin.coinconverter.data.model.ExchangeResponseValue
+import com.gfdellatin.coinconverter.data.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class ListExchangeUseCase(
+    private val repository: CoinRepository
+) : UseCase.NoParam<List<ExchangeResponseValue>>() {
+
+    override suspend fun execute(): Flow<List<ExchangeResponseValue>> {
+        return flow {
+            repository.list()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gfdellatin/coinconverter/domain/SaveExchangeUseCase.kt
@@ -0,0 +1,19 @@
+package com.gfdellatin.coinconverter.domain
+
+import com.gfdellatin.coinconverter.core.UseCase
+import com.gfdellatin.coinconverter.data.model.ExchangeResponseValue
+import com.gfdellatin.coinconverter.data.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SaveExchangeUseCase(
+    private val repository: CoinRepository
+) : UseCase.NoSource<ExchangeResponseValue>() {
+
+    override suspend fun execute(param: ExchangeResponseValue): Flow<Unit> {
+        return flow {
+            repository.save(param)
+        }
+    }
+
+}
