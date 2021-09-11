eb9f9a0edc6fcc9999afc9848de425d72e7ec706
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-11 17:13:11 -0300

    Some Adjustments and implements button usages

--- app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.gfdellatin.coinconverter.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
-import com.gfdellatin.coinconverter.core.extensions.text
+import com.gfdellatin.coinconverter.core.extensions.*
 import com.gfdellatin.coinconverter.data.model.Coin
 import com.gfdellatin.coinconverter.databinding.ActivityMainBinding
+import com.gfdellatin.coinconverter.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModel<MainViewModel>()
+    private val dialog by lazy { createProgressDialog() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         bindAdapters()
         bindListeners()
+        bindObserve()
     }
 
     private fun bindAdapters() {
@@ -27,8 +31,8 @@ class MainActivity : AppCompatActivity() {
         binding.tvFrom.setAdapter(adapter)
         binding.tvTo.setAdapter(adapter)
 
-        binding.tvFrom.setText(Coin.BRL.name, false)
-        binding.tvTo.setText(Coin.USD.name, false)
+        binding.tvFrom.setText(Coin.USD.name, false)
+        binding.tvTo.setText(Coin.BRL.name, false)
     }
 
     private fun bindListeners() {
@@ -37,7 +41,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnConverter.setOnClickListener {
-            Log.e("TAG", "bindListeners: " + binding.tilValue.text)
+            it.hideSoftKeyboard()
+            val search = "${binding.tilFrom.text}-${binding.tilTo.text}"
+            viewModel.getExchangeValue(search)
         }
+    }
+
+    private fun bindObserve() {
+        viewModel.state.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    dialog.dismiss()
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                }
+                is MainViewModel.State.Success -> success(it)
+            }
+        }
+    }
+
+    private fun success(it: MainViewModel.State.Success) {
+        dialog.dismiss()
+
+        val selectedCoin = binding.tilTo.text
+        val coin = Coin.values().find { it.name == selectedCoin } ?: Coin.BRL
+
+        val result = it.exchange.bid * binding.tilValue.text.toDouble()
+
+        binding.tvResult.text = result.formatCurrency(coin.locale)
     }
 }
