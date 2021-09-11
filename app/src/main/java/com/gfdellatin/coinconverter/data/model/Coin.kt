4e7bef95b88c2d47659ee79bce644ea742811cea
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-10 23:51:54 -0300

    Finish Main Layout

---
+++ app/src/main/java/com/gfdellatin/coinconverter/data/model/Coin.kt
@@ -0,0 +1,8 @@
+package com.gfdellatin.coinconverter.data.model
+
+enum class Coin {
+    USD,
+    CAD,
+    BRL,
+    ARS
+}
--- app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.gfdellatin.coinconverter.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.gfdellatin.coinconverter.R
+import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import com.gfdellatin.coinconverter.core.extensions.text
+import com.gfdellatin.coinconverter.data.model.Coin
+import com.gfdellatin.coinconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        bindAdapters()
+        bindListeners()
+    }
+
+    private fun bindAdapters() {
+        val list = Coin.values()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        binding.tvFrom.setAdapter(adapter)
+        binding.tvTo.setAdapter(adapter)
+
+        binding.tvFrom.setText(Coin.BRL.name, false)
+        binding.tvTo.setText(Coin.USD.name, false)
+    }
+
+    private fun bindListeners() {
+        binding.tilValue.editText?.doAfterTextChanged {
+            binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
+        }
+
+        binding.btnConverter.setOnClickListener {
+            Log.e("TAG", "bindListeners: " + binding.tilValue.text)
+        }
     }
 }
