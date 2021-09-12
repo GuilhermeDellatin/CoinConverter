84865d1f166eaba7a34de0a9ff081df372727117
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-11 22:05:27 -0300

    Adjusts

--- app/src/main/java/com/gfdellatin/coinconverter/data/model/Coin.kt
+++ app/src/main/java/com/gfdellatin/coinconverter/data/model/Coin.kt
@@ -6,5 +6,9 @@ enum class Coin(val locale: Locale) {
     USD(Locale.US),
     CAD(Locale.CANADA),
     BRL(Locale("pt", "BR")),
-    ARS(Locale("es", "AR"))
+    ARS(Locale("es", "AR"));
+
+    companion object {
+        fun getByName(name: String) = values().find { it.name == name} ?: BRL
+    }
 }
--- app/src/main/java/com/gfdellatin/coinconverter/data/model/ExchangeResponse.kt
+++ app/src/main/java/com/gfdellatin/coinconverter/data/model/ExchangeResponse.kt
@@ -1,20 +1,16 @@
 package com.gfdellatin.coinconverter.data.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 typealias ExchangeResponse = HashMap<String, ExchangeResponseValue>
 
 @Entity(tableName = "tb_exchange")
 data class ExchangeResponseValue (
+    @PrimaryKey(autoGenerate = true)
+    var id: Long,
     val code: String,
     val codein: String,
     val name: String,
-    val high: String,
-    val low: String,
-    val varBid: String,
-    val pctChange: String,
-    val bid: Double,
-    val ask: String,
-    val timestamp: String,
-    val createDate: String
+    val bid: Double
 )
