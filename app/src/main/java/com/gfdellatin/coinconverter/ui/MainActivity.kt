3b9f46f845d5d947886e8de781e2c7e44effe02e
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-11 22:12:21 -0300

    Main menu to show history activity

--- app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/coinconverter/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.gfdellatin.coinconverter.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
+import com.gfdellatin.coinconverter.R
 import com.gfdellatin.coinconverter.core.extensions.*
 import com.gfdellatin.coinconverter.data.model.Coin
 import com.gfdellatin.coinconverter.databinding.ActivityMainBinding
@@ -23,6 +27,18 @@ class MainActivity : AppCompatActivity() {
         bindAdapters()
         bindListeners()
         bindObserve()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_history) {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun bindAdapters() {
@@ -66,7 +82,7 @@ class MainActivity : AppCompatActivity() {
         dialog.dismiss()
 
         val selectedCoin = binding.tilTo.text
-        val coin = Coin.values().find { it.name == selectedCoin } ?: Coin.BRL
+        val coin = Coin.getByName(selectedCoin)
 
         val result = it.exchange.bid * binding.tilValue.text.toDouble()
 
